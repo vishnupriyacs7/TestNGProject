0b03f26a9e006bcd3b8dfe61da304539d44adca8
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-20 20:07:54 +0530

    priority

--- src/NewPackage/PriorityTestNG.java
+++ src/NewPackage/PriorityTestNG.java
@@ -1,34 +1,54 @@
 package NewPackage;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PriorityTestNG {
-	WebDriver driver;                  
-   	
-	@Test           	
-	public void openBrowser() {                     	
-    	driver = new ChromeDriver();                          
-	}       	
-            	
-	@Test         	
-	public void launchGoogle() {                            	
-        driver.get("https://www.google.com/");             	                         
-	}       	
-                    
-	@Test	
-	public void peformSeachAndClick1stLink() {                      	
-        driver.findElement(By.name("q")).sendKeys("Facebook");    
-  
-	}       	
-                  	
-//	@Test(priority=4)            	
-//	public void FaceBookPageTitleVerification() throws Exception {                          	
-//    	WebElement search =driver.findElement(By.name("q"));
-//    	search.submit();
-//    	Thread.sleep(3000);         	
-//        Assert.assertEquals(driver.getTitle().contains("Facebook - Google Search"), true);                              
-//	} 
+	WebDriver driver;
+
+    // Method 1: Open Browser and Initialize WebDriver
+    @Test(priority = 1)
+    public void openBrowser() {
+        driver = new ChromeDriver(); // Initialize ChromeDriver before any tests
+    }
+
+    // Method 2: Launch Google.com
+    @Test(priority = 2)
+    public void launchGoogle() {
+        driver.get("http://www.google.co.in");
+    }
+
+    // Method 3: Perform a search using "Facebook"
+    @Test(priority = 3)
+    public void performSearchAndClick1stLink() {
+        driver.findElement(By.name("q")).sendKeys("Facebook"); // Use 'name' for search input
+        driver.findElement(By.name("q")).submit(); // Submit the search form
+    }
+
+    // Method 4: Verify Google search page title
+    @Test(priority = 4)
+    public void FaceBookPageTitleVerification() {
+        // Wait for the title to contain 'Facebook - Google Search'
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleContains("Facebook - Google Search"));
+        
+        // Assert that the page title contains 'Facebook'
+        Assert.assertTrue(driver.getTitle().contains("Facebook - Google Search"));
+    }
+
+    // Method to close the browser after all tests are completed
+    @Test(priority = 5)
+    public void closeBrowser() {
+        if (driver != null) {
+            driver.quit(); // Close the browser after tests
+        }
+    }
+ 
 }

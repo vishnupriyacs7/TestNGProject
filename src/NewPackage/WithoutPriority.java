3bf144d35970276ffdaae6517f497e3b6702c13e
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-20 20:05:16 +0530

    priority

--- src/NewPackage/WithoutPriority.java
+++ src/NewPackage/WithoutPriority.java
@@ -3,7 +3,25 @@ package NewPackage;
 import org.testng.annotations.Test;
 
 public class WithoutPriority {
-  @Test
-  public void f() {
-  }
+	@Test()
+	public void c_method(){
+        	System.out.println("I'm in method C");
+	}
+	@Test()
+	public void b_method(){
+        	System.out.println("I'm in method B");
+	}
+	@Test(priority=6)
+	public void a_method(){
+        	System.out.println("I'm in method A");
+	}
+	@Test(priority=0)
+	public void e_method(){
+        	System.out.println("I'm in method E");
+	}
+	@Test(priority=6)
+	public void d_method(){
+        	System.out.println("I'm in method D");
+	}
+
 }

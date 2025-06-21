c4304691f46f92d21b97b6a9b0012362304c02df
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-21 23:02:12 +0530

    grouping

--- src/NewPackage/Grouping.java
+++ src/NewPackage/Grouping.java
@@ -3,7 +3,27 @@ package NewPackage;
 import org.testng.annotations.Test;
 
 public class Grouping {
-  @Test
-  public void f() {
-  }
+	@Test (groups = { "Car" })
+    public void Car1() {
+   		System.out.println("Batch Car - Test car 1");
+   	}
+   	
+   	@Test (groups = { "Car" })
+   	public void Car2() {
+   		System.out.println("Batch Car - Test car 2");
+   	}
+   	
+   	@Test (groups = { "Scooter" })
+    public void Scooter1() {
+   		System.out.println("Batch Scooter - Test scooter 1");
+   	}
+   	
+   	@Test (groups = { "Scooter" })
+    public void Scooter2() {
+   		System.out.println("Batch Scooter - Test scooter 2");
+   	}
+   	@Test (groups = { "Car", "Sedan Car" })
+   	public void Sedan1() {
+   		System.out.println("Batch Sedan Car - Test sedan 1");
+   	}
 }

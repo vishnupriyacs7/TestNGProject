60aa390608edea410e39d85df471bdc5b5174d3f
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-21 22:58:46 +0530

    parallel

--- src/NewPackage/SearchParallel.java
+++ src/NewPackage/SearchParallel.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-import mypackage.Parallel1;
+import NewPackage.Parallel1;
 
 public class SearchParallel  extends Parallel1  {
 	@Test

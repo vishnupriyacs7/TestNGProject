d11e89288eef655dafe4ead5ecf5d7f56c229b8a
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-21 22:56:44 +0530

    parallel

--- src/NewPackage/Parallel1.java
+++ src/NewPackage/Parallel1.java
@@ -1,9 +1,28 @@
 package NewPackage;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Parallel1 {
-  @Test
-  public void f() {
-  }
+	WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://google.com/"); 
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }

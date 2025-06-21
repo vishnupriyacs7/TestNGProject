ec64f32c9493fe524806c8ee7ae28a8e888de245
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-21 23:00:23 +0530

    parallel

--- src/NewPackage/AddToCartParallel.java
+++ src/NewPackage/AddToCartParallel.java
@@ -1,9 +1,20 @@
 package NewPackage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class AddToCartParallel {
-  @Test
-  public void f() {
-  }
+import NewPackage.Parallel1;
+
+public class AddToCartParallel extends Parallel1 {
+	@Test
+    public void addItemToCart() {
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Samsung Galaxy S24");
+        searchBox.submit();
+
+ 
+        System.out.println("Title" + driver.getTitle());
+        System.out.println(" Thread: " + Thread.currentThread().getId());
+    }
 }

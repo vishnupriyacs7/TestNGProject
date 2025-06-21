96711baf19a1cef2a74df106f7fc7b9f2140010d
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-21 22:58:15 +0530

    parallel

--- src/NewPackage/SearchParallel.java
+++ src/NewPackage/SearchParallel.java
@@ -1,9 +1,27 @@
 package NewPackage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class SearchParallel {
-  @Test
-  public void f() {
-  }
+import mypackage.Parallel1;
+
+public class SearchParallel  extends Parallel1  {
+	@Test
+    public void searchByProductName() {
+        WebElement searchBox = driver.findElement(By.name("q")); 
+        searchBox.sendKeys("iPhone 15");
+        searchBox.submit();
+
+        System.out.println("google Search " + Thread.currentThread().getId());
+    }
+
+    @Test
+    public void searchByCategory() {
+    	WebElement searchBox = driver.findElement(By.name("q"));         
+    	searchBox.sendKeys("scope india technopark");
+        searchBox.submit();
+
+        System.out.println("google Search " + Thread.currentThread().getId());
+    }
 }

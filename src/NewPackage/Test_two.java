06f5c6b421b453c613afdb5170f91957d61b3d64
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-22 23:55:22 +0530

    testng

--- src/NewPackage/Test_two.java
+++ src/NewPackage/Test_two.java
@@ -3,7 +3,13 @@ package NewPackage;
 import org.testng.annotations.Test;
 
 public class Test_two {
-  @Test
-  public void f() {
-  }
+	@Test
+    public void testMethodOne() {
+        System.out.println("Test Two - Method One - " + Thread.currentThread().getId());
+    }
+
+    @Test
+    public void testMethodTwo() {
+        System.out.println("Test Two - Method Two - " + Thread.currentThread().getId());
+    }
 }

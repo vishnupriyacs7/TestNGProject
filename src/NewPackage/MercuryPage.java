ffe32132d8e44590d25ba96505f5d8319daea022
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-22 23:48:50 +0530

    testng

--- src/NewPackage/MercuryPage.java
+++ src/NewPackage/MercuryPage.java
@@ -1,9 +1,22 @@
 package NewPackage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MercuryPage {
-  @Test
-  public void f() {
+	public String baseUrl = "http://demo.guru99.com/test/newtours/";
+	public WebDriver driver ;
+	
+	@Test
+	public void verifyHomepageTitle() {	   	
+      	System.out.println("launching browser");
+	  	driver = new ChromeDriver();
+	  	driver.get(baseUrl);
+	  	String expectedTitle = "Welcome: Mercury Tours";
+	  	String actualTitle = driver.getTitle();
+	  	Assert.assertEquals(actualTitle, expectedTitle);
+	  	driver.close();
   }
 }

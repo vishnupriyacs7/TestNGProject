f61e4604fadcb4b6f3396cac7f237737e398372f
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-23 12:04:57 +0530

    soft assertion

--- src/NewPackage/HardAssertion.java
+++ src/NewPackage/HardAssertion.java
@@ -7,48 +7,42 @@ public class HardAssertion {
 	@Test
 	public void assertEquals(){
 	 
-		Assert.assertEquals("assert will pass","assert will pass","error");
+		Assert.assertEquals("assert will pass","assert will pass","error");		 
+		System.out.println("This line is executed because assertEquals.");
 		 
-		System.out.println("This line is executed because assertEquals " + "passed since both the strings are same");
-		 
-		Assert.assertEquals("assertion","This assertion will fail");
-		 
-		System.out.println("This line will not be executed because " + "assertEquals fails both the strings are different."+ "Also the test/method will be declared failed");
+		Assert.assertEquals("assertion","This assertion will fail");		 
+		System.out.println("This line will not be executed.");
 	}
 	
 	@Test
 	public void assertNotEquals(){
 		Assert.assertNotEquals("This assertion will pass","Since the " + "expected and actual result do not match");
 		 
-		System.out.println("This line is executed because assertNotEquals" + " assertion pass for the given situation");
+		System.out.println("This line is executed.");
 	}
 	
 	@Test
 	public void assertFalse(){
-		Assert.assertFalse(3>5," 3 less than 5 true");
-		System.out.println("This line is executed because assertFalse" + "assertion passes as the given condition will return false");
+		Assert.assertFalse(3<5,"will fail");
+		System.out.println("assertFalse" );
 	}
 	
 	@Test
 	public void assertTrue(){
-		Assert.assertTrue(3<5," 3 less than 5 true");
-		System.out.println("This line is executed because assertFalse" + "assertion passes as the given condition will return false");
+		Assert.assertTrue(3<5);
+		System.out.println("assertTrue");
 	}
 	
 	@Test
 	public void assertNull(){
 		Assert.assertNull(null);
-		System.out.println("Since we we set null in the condition, the assertion " + "assertNull will pass");
+		System.out.println("assertNull will pass");
 	}
 	
 	@Test
 	public void assertNotNull(){
-		Assert.assertNotNull("This assertion will pass because this " + "string don't returns a null value");
-		System.out.println("This line is executed because assertNotNull have passed");
+		Assert.assertNotNull(5);
+		System.out.println("assertNotNull have passed");
 	}
-
-
-
-
 
 }
---
+++ src/NewPackage/SoftAssertion.java
@@ -0,0 +1,9 @@
+package NewPackage;
+
+import org.testng.annotations.Test;
+
+public class SoftAssertion {
+  @Test
+  public void f() {
+  }
+}

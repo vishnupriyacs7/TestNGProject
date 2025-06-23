7bc9948d596518e0f15399fddaa6a5c53e8b1356
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-23 11:53:06 +0530

    hard assertion

--- src/NewPackage/HardAssertion.java
+++ src/NewPackage/HardAssertion.java
@@ -11,9 +11,9 @@ public class HardAssertion {
 		 
 		System.out.println("This line is executed because assertEquals " + "passed since both the strings are same");
 		 
-//		Assert.assertEquals("assertion","This assertion will fail");
-//		 
-//		System.out.println("This line will not be executed because " + "assertEquals fails both the strings are different."+ "Also the test/method will be declared failed");
+		Assert.assertEquals("assertion","This assertion will fail");
+		 
+		System.out.println("This line will not be executed because " + "assertEquals fails both the strings are different."+ "Also the test/method will be declared failed");
 	}
 	
 	@Test

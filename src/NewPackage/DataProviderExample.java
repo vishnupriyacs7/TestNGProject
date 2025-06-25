c856643515cb298bd8b9fe14c23a4761215718f8
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-25 12:48:56 +0530

    dp inherit

--- src/NewPackage/DataProviderExample.java
+++ src/NewPackage/DataProviderExample.java
@@ -1,9 +1,20 @@
 package NewPackage;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DataProviderExample {
-  @Test
-  public void f() {
-  }
+	@DataProvider(name = "login-data")
+	public Object[][] loginData() {
+	    return new Object[][] {
+	        {"user1", "password1"},
+	        {"user2", "password2"},
+	        {"user3", "password3"}
+	    };
+	}
+
+	@Test(dataProvider = "login-data")
+	public void testLogin(String username, String password) {
+	    System.out.println("Testing login with Username: " + username + " and Password: " + password);
+	}
 }
